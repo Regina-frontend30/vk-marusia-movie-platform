e4e07d25c707fc7eb48352af4a9b1a0aeeaa15f7
Regina Frontend <reginakarimovna1330@gmail.com>
2026-04-21 23:49:59 +0600

    feat: implement top movies list

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,13 +1,44 @@
+import { useEffect, useState } from "react";
 import Hero from "../../widgets/hero/Hero";
 import TopMovies from "../../widgets/top-movies/TopMovies";
+import { getRandomMovie, getTopMovies } from "../../shared/api/movies";
+import type { Movie } from "../../shared/types/movie";
 
-function HomePage() {
+export default function HomePage() {
+  const [movie, setMovie] = useState<Movie | null>(null);
+  const [topMovies, setTopMovies] = useState<Movie[]>([]);
+
+  const loadRandom = async () => {
+    try {
+      const data = await getRandomMovie();
+      setMovie(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const loadTop = async () => {
+    try {
+      const data = await getTopMovies();
+      setTopMovies(data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    queueMicrotask(() => {
+      void loadRandom();
+      void loadTop();
+    });
+  }, []);
+
+  if (!movie) return <div>Загрузка...</div>;
+
   return (
     <>
-      <Hero />
-      <TopMovies />
+      <Hero movie={movie} onRefresh={loadRandom} />
+      <TopMovies movies={topMovies} />
     </>
   );
 }
-
-export default HomePage;
--- src/widgets/top-movies/TopMovies.tsx
+++ src/widgets/top-movies/TopMovies.tsx
@@ -1,21 +1,33 @@
-import MovieCard from "../../entities/movie/MovieCard";
-import { movies } from "../../entities/movie/model/movies";
+import type { Movie } from "../../shared/types/movie";
+import { useNavigate } from "react-router-dom";
 import "./TopMovies.scss";
 
-export default function TopMovies() {
+type Props = {
+  movies: Movie[];
+};
+
+export default function TopMovies({ movies }: Props) {
+  const navigate = useNavigate();
+
   return (
-    <section className="top-movies">
+    <section className="top-movies container">
       <h2 className="home__title">Топ-10 фильмов</h2>
 
       <div className="home__list">
-        {movies.map((movie) => (
-          <MovieCard
+        {movies.map((movie, index) => (
+          <div
             key={movie.id}
-            id={movie.id}
-            title={movie.title}
-            poster={movie.image}
-            position={movie.id}
-          />
+            className="movie-card"
+            onClick={() => navigate(`/movie/${movie.id}`)}
+          >
+            <span className="movie-card__position">{index + 1}</span>
+
+            <img
+              className="movie-card__image"
+              src={movie.posterUrl}
+              alt={movie.title}
+            />
+          </div>
         ))}
       </div>
     </section>
