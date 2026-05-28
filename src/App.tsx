0ff6036b7a613273bbcd2c5b20f22983618f2443
Regina Frontend <reginakarimovna1330@gmail.com>
2026-05-28 19:21:30 +0600

    feat: add genres page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./app/layouts/MainLayout";
+
 import HomePage from "./pages/home/HomePage";
 import MoviePage from "./pages/movie/MoviePage";
+import GenresPage from "./pages/genres/GenresPage";
 
 function App() {
   return (
     <MainLayout>
       <Routes>
         <Route path="/" element={<HomePage />} />
+
         <Route path="/movie/:id" element={<MoviePage />} />
-        <Route path="/login" element={<div>Login page</div>} />
-        <Route path="/genres" element={<div>Genres page</div>} />
+
+        <Route path="/genres" element={<GenresPage />} />
+
+        <Route path="/genres/:genre" element={<GenresPage />} />
       </Routes>
     </MainLayout>
   );
