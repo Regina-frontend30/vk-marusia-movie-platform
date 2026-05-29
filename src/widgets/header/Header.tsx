903bb4eb3276838908746c8b11fdf73def9fa567
Regina Frontend <reginakarimovna1330@gmail.com>
2026-05-29 19:41:44 +0600

    feat: open auth modal from header

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,29 +1,49 @@
-import "./Header.scss";
-import logo from "../../assets/logo/logo.svg";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
+import "./Header.scss";
+
+import logo from "../../assets/logo/logo.svg";
+import AuthModal from "../../features/auth/AuthModal";
+
 export default function Header() {
+  const [isAuthModalOpen, setIsAuthModalOpen] = useState(false);
+
   return (
-    <header className="header">
-      <div className="container header__inner">
-        {}
-        <Link to="/">
-          <img src={logo} alt="VK Marusia" className="header__logo" />
-        </Link>
-
-        <nav className="header__nav">
-          <Link to="/" className="header__link">Главная</Link>
-          <Link to="/genres" className="header__link">Жанры</Link>
-        </nav>
-
-        <div className="header__right">
-          <input className="header__search" placeholder="Поиск" />
-
-          <Link to="/login" className="header__link">
-            Войти
+    <>
+      <header className="header">
+        <div className="container header__inner">
+          <Link to="/">
+            <img src={logo} alt="VK Marusia" className="header__logo" />
           </Link>
+
+          <nav className="header__nav">
+            <Link to="/" className="header__link">
+              Главная
+            </Link>
+
+            <Link to="/genres" className="header__link">
+              Жанры
+            </Link>
+          </nav>
+
+          <div className="header__right">
+            <input className="header__search" placeholder="Поиск" />
+
+            <button
+              type="button"
+              className="header__link"
+              onClick={() => setIsAuthModalOpen(true)}
+            >
+              Войти
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      {isAuthModalOpen && (
+        <AuthModal onClose={() => setIsAuthModalOpen(false)} />
+      )}
+    </>
   );
 }
